abbcceb8f3714a74d64c9613bb36442f5d786b15
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-04 20:56:05 +0200

    feature: scss files add and configure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["@/assets/scss/index.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/_breakpoints.scss" as *;
+                           @use "@/assets/scss/_functions.scss" as *;
+                           @use "@/assets/scss/_mixins.scss" as *;`,
+        },
+      },
+    },
+  },
+});
