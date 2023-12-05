50dccd28ce601f3df664bb38c4ed2aaf62175e48
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-05 12:41:31 +0200

    feature: vite-svg-loader add to plugins in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import svgLoader from "vite-svg-loader";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -12,6 +14,7 @@ export default defineNuxtConfig({
         },
       },
     },
+    plugins: [svgLoader()],
   },
   modules: ["@pinia/nuxt"],
 });
