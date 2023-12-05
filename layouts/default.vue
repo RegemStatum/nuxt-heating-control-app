2a4c9ce00f55290962229d087a14d17414e237a5
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-05 11:20:45 +0200

    feature: app layout initialize

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <header>Header</header>
+
+    <main>
+      <slot></slot>
+    </main>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
