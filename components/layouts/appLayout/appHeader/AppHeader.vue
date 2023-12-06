1337dc272177cfa29805476cd0a328a8b1bfb6fa
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-06 14:40:01 +0200

    feature: app header add

---
+++ components/layouts/appLayout/appHeader/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { UserIcon } from "@heroicons/vue/24/outline";
+
+import AppHeaderLogo from "./AppHeaderLogo";
+</script>
+
+<template>
+  <header class="header">
+    <div class="inner-container">
+      <AppHeaderLogo />
+      <UserIcon />
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  @include boxShadow1;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  padding: getInd(400) 0;
+  background-color: getColor("nt-12");
+
+  .inner-container {
+    $containerHeight: 30px;
+    @include container;
+
+    display: flex;
+    justify-content: space-between;
+    height: $containerHeight;
+  }
+}
+</style>
