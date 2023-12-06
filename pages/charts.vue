1f925ea00bbc8455481410a8c92ee762bb1fb5d6
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-06 14:27:58 +0200

    feature: charts page add

---
+++ pages/charts.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Charts page</h1>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat molestias
+    vero laudantium optio blanditiis, aut velit quas quidem quasi. Eveniet.
+  </p>
+</template>
