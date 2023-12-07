5086a3aadf618a3dff2960586c0399f774174387
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-07 11:56:58 +0200

    update: auth layout to take the whole page and center elements on it

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,5 +1,18 @@
 <template>
-  <main>
+  <main class="main">
     <slot></slot>
   </main>
 </template>
+
+<style lang="scss">
+.main {
+  $pageWidth: 100vw;
+  $pageHeight: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: $pageWidth;
+  height: $pageHeight;
+}
+</style>
