ea967c41fea82c291c72b1236ab29e3802eceba2
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-07 11:58:02 +0200

    feature: authentication form initialize

---
+++ components/auth/AuthForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  authType: "signup" | "login";
+}
+const { authType } = defineProps<Props>();
+
+interface TemplateInfo {
+  heading: string;
+}
+const signupTemplateInfo: TemplateInfo = {
+  heading: "Sign up",
+};
+const loginTemplateInfo: TemplateInfo = {
+  heading: "Log in",
+};
+const templateInfo =
+  authType === "signup" ? signupTemplateInfo : loginTemplateInfo;
+</script>
+
+<template>
+  <div class="container">
+    
+    <h1>{{ templateInfo.heading }}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
