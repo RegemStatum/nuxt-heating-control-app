607a7d9eae0ceeeb60309f61213303fc6d1ee313
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-07 11:40:08 +0200

    feature: authentication layout add

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "auth",
+});
+</script>
+
 <template>
   <p>Login page</p>
 </template>
--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,3 +1,9 @@
+<script>
+definePageMeta({
+  layout: "auth",
+});
+</script>
+
 <template>
   <p>Signup page</p>
 </template>
