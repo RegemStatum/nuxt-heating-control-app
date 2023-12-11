a20194cb3f8ad61b08c7d895090409f30dc938af
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-11 13:02:11 +0200

    update: save user to local storage, login, logout functions in useFirebaseAuth composable

--- composables/useFirebaseAuth.ts
+++ composables/useFirebaseAuth.ts
@@ -1,9 +1,35 @@
-import { createUserWithEmailAndPassword, type User } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  type User,
+  type UserCredential,
+} from "firebase/auth";
+import LocalStorageItems from "@/constants/localStorage";
 
 const useFirebaseAuth = () => {
   const { $auth } = useNuxtApp();
 
   const user = useState<User | null>("user", () => null);
+
+  const checkIsUserInLocalStorage = () => {
+    const localStorageUser = localStorage.getItem(LocalStorageItems.User);
+    return !!localStorageUser;
+  };
+
+  const setUserFromLocalStorage = () => {
+    const localStorageUser = localStorage.getItem(LocalStorageItems.User);
+    if (localStorageUser) {
+      const userData = JSON.parse(localStorageUser);
+      user.value = userData;
+    }
+  };
+
+  const setUser = (userCredentials: UserCredential) => {
+    const userData = userCredentials.user;
+    user.value = userData;
+    localStorage.setItem(LocalStorageItems.User, JSON.stringify(userData));
+  };
 
   const registerUser = async (email: string, password: string) => {
     const userCredentials = await createUserWithEmailAndPassword(
@@ -12,13 +38,33 @@ const useFirebaseAuth = () => {
       password
     );
     if (userCredentials) {
-      user.value = userCredentials.user;
+      setUser(userCredentials);
     }
+  };
+
+  const loginUser = async (email: string, password: string) => {
+    const userCredentials = await signInWithEmailAndPassword(
+      $auth,
+      email,
+      password
+    );
+    if (userCredentials) {
+      setUser(userCredentials);
+    }
+  };
+
+  const logoutUser = async () => {
+    await signOut($auth);
+    localStorage.setItem(LocalStorageItems.User, "");
   };
 
   return {
     user,
     registerUser,
+    loginUser,
+    logoutUser,
+    setUserFromLocalStorage,
+    checkIsUserInLocalStorage,
   };
 };
 
