4ade97c2470c7c682c2b46607fd55e2b74265d85
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-12-12 11:12:43 +0200

    update: user page split to components

---
+++ components/user/UserControl.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { user } = useFirebaseAuth();
+</script>
+
+<template>
+  <div class="container">
+    <p>User: {{ user?.email }}</p>
+    <UserLogout />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: getInd(200);
+}
+</style>
--- pages/user.vue
+++ pages/user.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
-const { logoutUser } = useFirebaseAuth();
-
-const handleUserLogout = async () => {
-  try {
-    await logoutUser();
-    navigateTo("/auth/login");
-  } catch (error) {
-    console.error(error);
-  }
-};
 </script>
 
 <template>
   <PagesPageTitle title="User" />
-  <button @click="handleUserLogout">Log out</button>
+  <UserControl />
 </template>
